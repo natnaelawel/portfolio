1cbb64f390d7781fa2ca6ed6e2b9305328837e3e
natnaelawel <natnael.awel@gmail.com>
2020-11-02 22:48:43 +0300

    scrollbar styles

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./components/Home/Home";
 import "./App.css";
-// import { gsap } from "gsap";
-// import { RoughEase } from "gsap/EasePack";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import { ScrollToPlugin } from "gsap/all";
+import { gsap } from "gsap";
+import { RoughEase } from "gsap/EasePack";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/all";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Portfolio from "./components/Portfolio/Portfolio";
 // import { useMediaQuery, useMediaQueries } from "@react-hook/media-query";
 
 function App(props) {
-  // console.log(props);
-  // let matches = false 
-  // matches = useMediaQuery("(min-width: 400px)");
-  // console.log('is matches', matches)
-  // gsap.registerPlugin(ScrollTrigger);
-  // gsap.registerPlugin(ScrollToPlugin);
-  // gsap.registerPlugin(RoughEase);
-  
-  // useEffect(()=>{
-  // })
+  const [matches, setMatches] = useState(false);
+  console.log(props);
+  // matches = window.matchMedia("(max-width: 700px)");
 
-  // const goToSection = (i, anim) => {
-  //   const firstElem = document.querySelector(".panel");
-  //   // let innerHeight = panel.clientHeight;
-  //   console.log("current ", i);
-  //   let panels = gsap.utils.toArray(".panel");
-  //   let innerHeight = panels[i].clientHeight;
-  //   console.log("inner height is ", innerHeight);
-  //   gsap.to(window, {
-  //     scrollTo: { y: i * innerHeight + firstElem.offsetTop, autoKill: false },
-  //     duration: 1,
-  //   });
+  console.log("is matches", matches);
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollToPlugin);
+  gsap.registerPlugin(RoughEase);
 
-  //   if (anim) {
-  //     anim.restart();
-  //   }
-  // };
+  useEffect(() => {
+    let isMatch = window.matchMedia("(max-width: 700px)");
+    setMatches(isMatch);
+  });
+
+  const goToSection = (i, anim) => {
+    const firstElem = document.querySelector(".panel");
+    // let innerHeight = panel.clientHeight;
+    console.log("current ", i);
+    let panels = gsap.utils.toArray(".panel");
+    let innerHeight = panels[i].clientHeight;
+    console.log("inner height is ", innerHeight);
+    gsap.to(window, {
+      scrollTo: { y: i * innerHeight + firstElem.offsetTop, autoKill: false },
+      duration: 1,
+    });
+
+    if (anim) {
+      anim.restart();
+    }
+  };
   // useEffect(() => {
-  //     // gsap.set("body", { overflow: "hidden" });
-
+  //   // gsap.set("body", { overflow: "hidden" });
+  //   if (matches) {
   //     gsap.utils.toArray(".panel").forEach((panel, i) => {
   //       ScrollTrigger.create({
   //         trigger: panel,
@@ -52,7 +55,8 @@ function App(props) {
   //         onEnterBack: () => goToSection(i),
   //       });
   //     });
-  // }, []);
+  //   }
+  // }, [matches]);
   return (
     <div>
       <BrowserRouter>
