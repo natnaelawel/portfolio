93213273fc3b3bfe2081168190e39fee5b1055f1
natnaelawel <natnael.awel@gmail.com>
2020-11-02 23:45:16 +0300

    scrollbar and modal animation

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+// import React, { useEffect, useState } from "react";
 import Home from "./components/Home/Home";
 import "./App.css";
-import { gsap } from "gsap";
-import { RoughEase } from "gsap/EasePack";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollToPlugin } from "gsap/all";
+// import { gsap } from "gsap";
+// import { RoughEase } from "gsap/EasePack";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { ScrollToPlugin } from "gsap/all";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Portfolio from "./components/Portfolio/Portfolio";
 // import { useMediaQuery, useMediaQueries } from "@react-hook/media-query";
 
 function App(props) {
-  const [matches, setMatches] = useState(false);
-  console.log(props);
+
+  // const [matches, setMatches] = useState(false);
+  // console.log(props);
   // matches = window.matchMedia("(max-width: 700px)");
 
-  console.log("is matches", matches);
-  gsap.registerPlugin(ScrollTrigger);
-  gsap.registerPlugin(ScrollToPlugin);
-  gsap.registerPlugin(RoughEase);
+  // console.log("is matches", matches);
+  // gsap.registerPlugin(ScrollTrigger);
+  // gsap.registerPlugin(ScrollToPlugin);
+  // gsap.registerPlugin(RoughEase);
 
-  useEffect(() => {
-    let isMatch = window.matchMedia("(max-width: 700px)");
-    setMatches(isMatch);
-  });
+  // useEffect(() => {
+  //   let isMatch = window.matchMedia("(max-width: 700px)");
+  //   setMatches(isMatch);
+  // });
 
-  const goToSection = (i, anim) => {
-    const firstElem = document.querySelector(".panel");
-    // let innerHeight = panel.clientHeight;
-    console.log("current ", i);
-    let panels = gsap.utils.toArray(".panel");
-    let innerHeight = panels[i].clientHeight;
-    console.log("inner height is ", innerHeight);
-    gsap.to(window, {
-      scrollTo: { y: i * innerHeight + firstElem.offsetTop, autoKill: false },
-      duration: 1,
-    });
+  // const goToSection = (i, anim) => {
+  //   const firstElem = document.querySelector(".panel");
+  //   // let innerHeight = panel.clientHeight;
+  //   console.log("current ", i);
+  //   let panels = gsap.utils.toArray(".panel");
+  //   let innerHeight = panels[i].clientHeight;
+  //   console.log("inner height is ", innerHeight);
+  //   gsap.to(window, {
+  //     scrollTo: { y: i * innerHeight + firstElem.offsetTop, autoKill: false },
+  //     duration: 1,
+  //   });
 
-    if (anim) {
-      anim.restart();
-    }
-  };
+  //   if (anim) {
+  //     anim.restart();
+  //   }
+  // };
   // useEffect(() => {
   //   // gsap.set("body", { overflow: "hidden" });
   //   if (matches) {
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,15 +5,30 @@ import Header from "../Header/Header";
 import MyModal from "../Modal/Modal";
 import Services from "../Services/Services";
 import "./styles.css";
+import { gsap } from "gsap";
+import { useEffect } from "react";
 
 function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const fadeIn = () => {
+    const modalBox = document.querySelector(".modal__box");
+    console.log(modalBox)
+    let tl = gsap.timeline();
+    tl.from(modalBox, {
+      x: 1000,
+      opacity: 0,
+      duration: 1,
+      ease: "elastic.InOut",
+    });
+  };
+  useEffect(() => {
+    fadeIn()
+  }, [isModalOpen])
   return (
     <div id="get__in_touch_modal" className="home">
       <Header setIsModalOpen={setIsModalOpen} />
       {isModalOpen ? (
-        <MyModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+        <MyModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} /> 
       ) : (
         <>
           <Services />
